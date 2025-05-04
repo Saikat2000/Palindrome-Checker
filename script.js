bff07469a39a19bb893954141f2dcfc827e8cd30
Saikat De <81856219+Saikat2000@users.noreply.github.com>
2025-05-04 22:24:11 +0530

    feat: check whether a string is palindrome

---
+++ script.js
@@ -0,0 +1,20 @@
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+checkBtn.addEventListener("click", function () {
+    
+    const textInput = document.getElementById("text-input").value;
+    isPalindrome(textInput);
+});
+function isPalindrome(str) {
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    const reverseStr = cleanStr.split("").reverse().join("");
+    
+    if(cleanStr === reverseStr){
+        result.style.display = "block";
+        result.textContent = `${str} is a Palindrome`;
+    } else {
+        result.style.display = "block";
+        result.textContent = `${str} is not a Palindrome`;
+    }
+}
